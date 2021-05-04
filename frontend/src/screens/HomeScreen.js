fcdd0af65f234970948cbff837856ec2ff55b296
pquoc693 <pquoc693@gmail.com>
2021-05-04 00:55:35 -0700

    commit5

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,30 @@
-import Reat from 'react';
-import data from '../data.js';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import data from '../data.js';
 import Product from '../components/Product';
-export default function HomeScreen(){
-    return (<div>
-        <div className="row center">
-          {
-            data.products.map(product =>(
-              <Product key={product._id} product={product}/>
-            ))
-          }
-          </div>
-    </div>)
+import LoadingBox from '../components/LoadingBox.js';
+import MessageBox from '../components/MessageBox.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+export default function HomeScreen() {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading = false, products = [], error = null } = productList ? productList : {};
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (<div>
+    {loading ? (
+      <LoadingBox></LoadingBox>
+    ) : error ? (
+      <MessageBox variant="danger">{error}</MessageBox>
+    ) : (
+      <div className="row center">
+        {products.map((product) => (
+          <Product key={product._id} product={product}></Product>
+        ))}
+      </div>
+    )}
+  </div>)
 }
---
+++ frontend/src/store.js
@@ -0,0 +1,23 @@
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { productReducers } from './reducers/productReducers';
+
+import data from './data';
+
+const initialState = {};
+const reducer = combineReducers({
+  productList: productReducers,
+});
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const reducer = (state, action) =>{
+//   return {products: data.products};
+// }
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;
