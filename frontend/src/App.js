96191e1686da312e3d09a6bc3a2ecd7a86b15edb
pquoc693 <pquoc693@gmail.com>
2021-05-07 15:33:42 -0700

    commit7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import {
     BrowserRouter,
     // Switch,
@@ -10,16 +12,23 @@ import {
 } from "react-router-dom";
 import CartScreen from './screens/CartScreen';
 function App() {
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
     return (
         <BrowserRouter>
             <div className="grid-container">
                 <header className="row">
                     <div>
-                        <a className="brand" href="index.html">amazona</a>
+                        <Link className="brand" to="/"></Link>
                     </div>
                     <div>
-                        <a href="cart.html">Cart</a>
-                        <a href="signin.html">Sign In</a>
+                        <Link to="/cart">
+                            Cart
+                                {cartItems.length > 0 && (
+                                <span className="badge">{cartItems.length}</span>
+                            )}
+                        </Link>
+                        <Link to="/signin">Sign In</Link>
                     </div>
                 </header>
                 <main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,22 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import {
     productReducers,
     productDetailsReducer,
 } from './reducers/productReducers';
 
-import data from './data';
-
-const initialState = {};
+const initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+    },
+};
 const reducer = combineReducers({
     productList: productReducers,
     productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
