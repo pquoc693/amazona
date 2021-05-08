5c98b6b0f0564b32cb24998e34c0649bc4996813
pquoc693 <pquoc693@gmail.com>
2021-05-07 18:09:56 -0700

    commit10

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useNewUrlParser: true,
@@ -9,6 +9,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useCreateIndex: true,
 });
 const app = express();
+app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
@@ -16,18 +17,6 @@ app.get('/', (req, res) => {
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
-});
-
-
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
-app.get('/api/products/:id', (req, res) => {
-    const id = req.params.id;
-    const product = data.products.find((x) => x._id === id);
-    if (product)
-        res.send(product);
-    else res.status(404).send({ message: 'Product Not Found' });
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
