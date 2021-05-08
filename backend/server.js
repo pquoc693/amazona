2f03cd755c83301f6a5f96c8b9084aac00329ddb
pquoc693 <pquoc693@gmail.com>
2021-05-07 18:48:44 -0700

    commit11

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 
@@ -8,16 +9,21 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useUnifiedTopology: true,
     useCreateIndex: true,
 });
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
-});
+// app.use((err, req, res, next) => {
+//     res.status(500).send({ message: err.message });
+// });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
